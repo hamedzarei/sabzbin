b8a4f84513edb0d15fc6155957c21742ce92efc8
Hamed Zarei <hamedzarei.7232@gmail.com>
2020-12-12 02:54:11 +0330

    serializer, reverse relation

--- api/users/models.py
+++ api/users/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 from django_enum_choices.fields import EnumChoiceField
 from enum import Enum
+# from rest_framework import serializers
 from django.core import serializers
+from django.db.models import Count
 
 # Create your models here.
 class UserType(Enum):
@@ -16,6 +18,14 @@ class User(AbstractUser):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
+    def address_count(self, query_set):
+        return query_set.annotate(Count('address'))
+
+    def public_user_serializer(self, query_set):
+        return serializers.serialize('json', query_set, fields={'first_name', 'last_name'})
+    def publics_user_serializer(self, query_set):
+        return serializers.serialize('json', self.address_count(query_set), fields={'first_name', 'last_name', 'address_count'})
+
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.TextField(max_length=100,blank=True)
@@ -26,15 +36,13 @@ class Address(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
 
     def support(self):
-        items = Address.objects.all()
-        # print(items)
-        # i = serializers.serialize('json', Address.objects.all(), fields=('name','type'))
+        items = Address.objects.filter(type=UserType.SUPPORT).values()
         return items
 
-class PublicUser():
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name']
+# class PublicUser(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['first_name', 'last_name']
 
 
 
--- api/users/views.py
+++ api/users/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
-from users.models import Address, UserType, User, PublicUser
+from users.models import Address, UserType, User
 from response import error_response, ok_response
 from rest_framework import viewsets
-
-# Create your views here.
-class UsersViewSet(viewsets.ModelViewSet):
-    # define queryset
-    queryset = User.objects.all()
-
-    # specify serializer to be used
-    serializer_class = PublicUser
+from django.db.models import Count
 
 def index(request):
     # a = Address(name='home2', location='12354', user_id=1, type=UserType.USER)
     # a.save()
     user = User()
-    return ok_response(user.objects.all())
+    # a = Address()
+    # a.support()
+    # print(User.objects.values())
+    user = User()
+    return ok_response(user.publics_user_serializer(User.objects.all()))
+    # return ok_response(PublicUser(User.objects))
+    return ok_response(User.objects.annotate(Count('address')).values())
     address = Address()
     # return
     return ok_response(address.support())
